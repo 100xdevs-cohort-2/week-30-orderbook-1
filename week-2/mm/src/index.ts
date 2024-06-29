e51e24bc21b77a88d8c8b0eabec1f8f77fc46b84
harkirat <harkirat96@gmail.com>
2024-06-29 18:03:37 +0530

    removed logs

--- week-2/mm/src/index.ts
+++ week-2/mm/src/index.ts
@@ -8,24 +8,20 @@ const USER_ID = "5";
 
 async function main() {
     const price = 1000 + Math.random() * 10;
-
-    console.log("before getting open orders");
     const openOrders = await axios.get(`${BASE_URL}/api/v1/order/open?userId=${USER_ID}&market=${MARKET}`);
-    console.log("after getting open orders");
 
     const totalBids = openOrders.data.filter((o: any) => o.side === "buy").length;
     const totalAsks = openOrders.data.filter((o: any) => o.side === "sell").length;
-    console.log("about to cancel few bids and asks")
+
     const cancelledBids = await cancelBidsMoreThan(openOrders.data, price);
     const cancelledAsks = await cancelAsksLessThan(openOrders.data, price);
-    console.log("cancelled few bids and asks")
+
 
     let bidsToAdd = TOTAL_BIDS - totalBids - cancelledBids;
     let asksToAdd = TOTAL_ASK - totalAsks - cancelledAsks;
 
     while(bidsToAdd > 0 || asksToAdd > 0) {
         if (bidsToAdd > 0) {
-            console.log("Adding bid");
             await axios.post(`${BASE_URL}/api/v1/order`, {
                 market: MARKET,
                 price: (price - Math.random() * 1).toFixed(1).toString(),
@@ -36,7 +32,6 @@ async function main() {
             bidsToAdd--;
         }
         if (asksToAdd > 0) {
-            console.log("Adding ask");
             await axios.post(`${BASE_URL}/api/v1/order`, {
                 market: MARKET,
                 price: (price + Math.random() * 1).toFixed(1).toString(),
@@ -47,7 +42,6 @@ async function main() {
             asksToAdd--;
         }
     }
-    console.log("Done with one iteration");
 
     await new Promise(resolve => setTimeout(resolve, 1000));
 
@@ -56,7 +50,6 @@ async function main() {
 
 async function cancelBidsMoreThan(openOrders: any[], price: number) {
     let promises: any[] = [];
-    console.log("Cancelling bids more than " + price);
     openOrders.map(o => {
         if (o.side === "buy" && (o.price > price || Math.random() < 0.1)) {
             promises.push(axios.delete(`${BASE_URL}/api/v1/order`, {
@@ -67,15 +60,12 @@ async function cancelBidsMoreThan(openOrders: any[], price: number) {
             }));
         }
     });
-    console.log("awaiting promise")
-    console.log(promises.length);
     await Promise.all(promises);
     return promises.length;
 }
 
 async function cancelAsksLessThan(openOrders: any[], price: number) {
     let promises: any[] = [];
-    console.log("Cancelling asks less than " + price);
     openOrders.map(o => {
         if (o.side === "sell" && (o.price < price || Math.random() < 0.5)) {
             promises.push(axios.delete(`${BASE_URL}/api/v1/order`, {
@@ -87,8 +77,6 @@ async function cancelAsksLessThan(openOrders: any[], price: number) {
         }
     });
 
-    console.log("awaiting promise ask")
-    console.log(promises.length);
     await Promise.all(promises);
     return promises.length;
 }
