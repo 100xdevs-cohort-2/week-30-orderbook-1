ffb5d4de4a3d51ecdce31a60cf9eb3d03bf87abc
mohitejaikumar <mohitjaikumar@gmail.com>
2024-06-29 12:47:50 +0530

    added nice scrollbar

--- week-1/day-2/binance-frontend-realtime/app/components/depth/Depth.tsx
+++ week-1/day-2/binance-frontend-realtime/app/components/depth/Depth.tsx
@@ -71,16 +71,19 @@ export function DepthComponent({ market }: {market: string}) {
         }
     }, [market])
     
-    return <div className="h-[500px] relative overflow-y-scroll no-scrollbar ">
+    return <div className="relative">
         <TableHeader />
-        {asks && <AskTable asks={asks} />}
-        {price && <div>{price}</div>}
-        {bids && <BidTable bids={bids} />}
+        <div className="h-[500px] relative overflow-y-scroll no-scrollbar">
+            {asks && <AskTable asks={asks} />}
+            {price && <div>{price}</div>}
+            {bids && <BidTable bids={bids} />}
+        </div>
+        
     </div>
 }
 
 function TableHeader() {
-    return <div className="absolute top-0 left-0 w-full flex justify-between text-xs">
+    return <div className="absolute top-0 left-0 w-full h-5 flex justify-between text-xs bg-black z-10">
     <div className="text-white">Price</div>
     <div className="text-slate-500">Size</div>
     <div className="text-slate-500">Total</div>
